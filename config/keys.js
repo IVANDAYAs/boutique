14415244be971f1bf30c23cb0bdfa4dbfee52038
DAYALAN S <64883574+IVANDAYAs@users.noreply.github.com>
2023-11-10 14:13:15 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,5 @@
 module.exports = {
     mongoURI: "mongodb+srv://daya:daya@cluster0.vwiok.mongodb.net/boutique?retryWrites=true&w=majority",
-    mongodb+srv://daya:<password>@cluster0.vwiok.mongodb.net/?retryWrites=true&w=majority
     secretOrKey: "secret",
     RAZOR_PAY_KEY_ID: "rzp_test_O0X0x6mWJwqD7S",
     RAZOR_PAY_KEY_SECRET: "wcOG92eFBBZxHrO0lF7fWW7S"
